e4b1a9eadf1ce90d6d724a6f633a506349237941
danm0e <DesignIncWebMac@Daniels-MacBook-Pro-4.local>
2015-10-13 17:40:57 +0100

    Structure.01
    Initial responsive column structure and masthead

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,26 @@
 	@author:	dan moe & chris grace
 /************************************************************************/
 
+$(document).ready(function() {
+/*----------------------------------------------------------------------*/	
+
+	triggerOffset = $('main').offset().top
+	// console.log('Trigger is at ' + triggerOffset )
+
+	$(window).scroll(function () {
+		windowOffset = $(this).scrollTop()
+		// console.log('Window is at ' + windowOffset )
+
+	    if( windowOffset >= triggerOffset ) {
+			// console.log('YEP!')
+	    	// $('#nav').addClass('fixed-left')
+	    } else {
+	    	// $('#nav').removeClass('fixed-left')
+	    }
+
+	})
+
+
+
+/*----------------------------------------------------------------------*/	
+}) // END doc ready
