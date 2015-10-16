5ab17214de8f699b93eaeacf1702c036a943968f
danm0e <DesignIncWebMac@Daniels-MacBook-Pro-4.local>
2015-10-16 16:32:56 +0100

    Bootstrap & landing page
    Stripped out alternative grid system and rebuilt with Bootstrap.
    Finalised landing page structure and navigation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,8 @@
 $(document).ready(function() {
 /*----------------------------------------------------------------------*/	
 
+	// FIX NAVIGATION
+	/********************************************************************/
 	triggerOffset = $('main').offset().top
 	// console.log('Trigger is at ' + triggerOffset )
 
@@ -24,6 +26,44 @@ $(document).ready(function() {
 	    }
 
 	})
+
+	// ARROW JUMP NAVIGATION
+	/********************************************************************/
+	// cache each link with a hashtag
+	$('a[href*=#]').each(function() {
+		$(this).click(function(e) {
+			e.preventDefault()
+			var targetOffset = $($(this).attr('href')).offset().top
+			$('html, body, main').animate({ scrollTop: targetOffset }, 1000, 'swing')
+			// console.log('page scrolling to ' + $(this).attr('href'))
+		})
+	})
+
+    // LAZY LOADING
+	/********************************************************************/
+	
+	// call lazy load function on page load
+	lazyload()
+
+	// call lazy load function on scroll
+    $(window).scroll( function() {
+    	lazyload()
+    })
+
+    function lazyload() {
+    	// cache each lazyload object
+        $('.fadein').each( function(){
+        	// cache viewport height and object offset
+        	var windowBottom = $(window).scrollTop() + $(window).height(),
+        	objectBottom = $(this).offset().top + $(this).outerHeight()
+            // if the object is completely visible in the window
+            if( windowBottom > objectBottom ){
+                // fade it in 
+                $(this).animate({'opacity':'1', 'margin-top':'0'}, 500, 'swing')
+                // non js fallback set in globals.scss
+            }
+        })
+    }
 
 
 
