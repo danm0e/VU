a9c266bc4e0605bd8a63fb3afcca76e92fb20bee
danm0e <DesignIncWebMac@Daniels-MacBook-Pro-4.local>
2015-10-19 17:47:44 +0100

    Nav, Lazyload and initial content
    Bug fixes with navigation, includes fixed top header hero image. Add
    initial content and styling for sections – up until benefits.

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,10 +34,14 @@ $(document).ready(function() {
 		$(this).click(function(e) {
 			e.preventDefault()
 			var targetOffset = $($(this).attr('href')).offset().top
-			$('html, body, main').animate({ scrollTop: targetOffset }, 1000, 'swing')
+			$('html, body').animate({ scrollTop: targetOffset }, 1000, 'swing')
 			// console.log('page scrolling to ' + $(this).attr('href'))
 		})
 	})
+	// make sure the links to the top go to the absolute top of the page
+    $('a[href=#top]').click(function() {
+    	$('html, body').animate({ scrollTop: 0 }, 1000, 'swing')
+    })
 
     // LAZY LOADING
 	/********************************************************************/
@@ -55,9 +59,9 @@ $(document).ready(function() {
         $('.fadein').each( function(){
         	// cache viewport height and object offset
         	var windowBottom = $(window).scrollTop() + $(window).height(),
-        	objectBottom = $(this).offset().top + $(this).outerHeight()
+        	objectBottom = $(this).offset().top //+ $(this).outerHeight()
             // if the object is completely visible in the window
-            if( windowBottom > objectBottom ){
+            if( windowBottom >= objectBottom ){
                 // fade it in 
                 $(this).animate({'opacity':'1', 'margin-top':'0'}, 500, 'swing')
                 // non js fallback set in globals.scss
@@ -65,7 +69,28 @@ $(document).ready(function() {
         })
     }
 
+    // DARK LOGO FADE IN
+	/********************************************************************/
 
+    $(window).scroll( function() {
+		var windowTop = $(window).scrollTop(),
+			mainTop = $('main').offset().top
+
+			// change states on scroll down
+            if( windowTop >= mainTop ) {
+            	$('.dark-logo').addClass('in')
+            	$('#sidenav').addClass('push-down')
+	            $('.nav-wrapper').addClass('fixed-left')
+			}
+
+			// return to original states on scroll up
+			if ( windowTop <= mainTop ) {
+            	$('.dark-logo').removeClass('in')
+	            $('.nav-wrapper').removeClass('fixed-left')
+            	$('#sidenav').removeClass('push-down')
+			}
+
+    })
 
 /*----------------------------------------------------------------------*/	
 }) // END doc ready
